51e96b901a78e961f76b30185c414ae6d95fc0c2
viettungvuong <viettungvuong@gmail.com>
2023-04-12 20:40:47 +0700

    Numpy

--- gdp.py
+++ gdp.py
@@ -12,16 +12,9 @@ df = pd.read_csv('gdp.csv',index_col='Country Name')
 years=[]
 
 year_cols = [str(yr) for yr in range(2012, 2021)] # lấy tất cả các năm
+
+vietnam = df.loc[df['Country Code'] == 'VNM']
 for i in year_cols:
-    years.append(df[i].to_numpy())
-
-print(years[0][0])
-print(type(years[0][0]))
-
-
-
-for i in range(len(years)):
-    years[i][years[i] == '..'] = 0
-    for j in range(len(years[i])):
-        years[i][j]=Decimal(float(years[i][j]))
-
+    years.append(vietnam[i].to_numpy())
+print(len(years))
+print(years)
